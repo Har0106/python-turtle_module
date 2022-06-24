668e350f232e6f801e80a60c5a923f68a2813452
Har0106 <freddiemaron@gmail.com>
2022-06-24 19:19:30 +0530

    initial commit

---
+++ Snake_Game.py
@@ -0,0 +1,50 @@
+import turtle
+import random
+
+screen = turtle.Screen()
+
+snake = turtle.Turtle()
+snake.shape('square')
+snake.penup()
+
+class Move():
+    def __init__(self):
+        self.dis = 0
+    def up(self):
+        snake.setheading(90)
+        snake.forward(20)
+        self.dis = snake.pos()
+    def down(self):
+        snake.setheading(270)
+        snake.forward(20)
+        self.dis = snake.pos()
+    def left(self):
+        snake.setheading(180)
+        snake.forward(20)
+        self.dis = snake.pos()
+    def right(self):
+        snake.setheading(0)
+        snake.forward(20)
+        self.dis = snake.pos()
+
+food = turtle.Turtle()
+food.shape('circle')
+food.penup()
+food.shapesize(0.5)
+food.setpos(random.randrange(-200,200, step=20), random.randrange(-200,200, step=20))
+
+move = Move()
+turtle.listen()
+turtle.onkey(move.up, 'Up')
+turtle.onkey(move.down, 'Down')
+turtle.onkey(move.left, 'Left')
+turtle.onkey(move.right, 'Right')
+
+while True:
+    screen.update()
+    # print(move.dis, food.pos())
+    if move.dis == food.pos():
+        food.hideturtle()
+        break
+
+turtle.done()
